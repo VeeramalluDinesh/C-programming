c7e33d875abef64328c40ae6cffad8ecd9ee8afd
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-03-07 22:54:21 +0530

    Add files via upload

---
+++ dinner.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+int you;
+int date;
+printf("Enter the stylish of you:\n");
+scanf("%d",&you);
+printf("Enter the stylish of your date :\n");
+scanf("%d",&date);
+{
+if(you<=2 || date<=2)
+{
+ printf("0");
+ }
+else if(you>=8 || date>=8)
+{
+printf("2");
+}
+else{
+printf("1");
+}
+return 0;
+}
+}
+    
